eb70b1eb788fa7d41c49342200f7fb879cf969ca
Zerotens <12968743+zerotens@users.noreply.github.com>
2026-04-27 19:02:12 +0200

    up

--- home/default.nix
+++ home/default.nix
@@ -15,10 +15,6 @@
   home.stateVersion  = "25.11";
 
   # ── Sub-module imports ─────────────────────────────────────────────────────
-  programs.noctalia-shell = {
-    enable  = true;
-    package = pkgs.unstable.noctalia-shell;
-  };
 
   imports = [
     # User-installed applications (the main focus of this file)
